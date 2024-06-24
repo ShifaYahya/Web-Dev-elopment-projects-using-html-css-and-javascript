283a81167cd060d030e7ba56e983c27a981de4d4
Shifa <shifayahya327@gmail.com>
2024-06-24 15:36:46 +0530

    Add files via upload

---
+++ lamp website(none-responsive)/index.js
@@ -0,0 +1,6 @@
+var btnEl = document.querySelector(".btn")
+const lightEl = document.querySelector(".light-img")
+function toggleBtn(){
+    btnEl.classList.toggle("active")
+    lightEl.classList.toggle("on")
+}
